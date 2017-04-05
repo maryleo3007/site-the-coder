4e656adf9e1b5bcb4f586be12fad86e72024d739
Marilu Llamocca <maryleo.3007@gmail.com>
2017-04-05 11:07:24 -0500

    ejercicio leccion 19 enunciado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,56 @@
 function PostCoder(){
   this.posts = [];
   this.postCount = 0;
-  this.addPost = function(txtPost){
-    this.posts.push({id: this.postCount,
-                     textPost: txtPost});
-    this.postCount++;
+  this.addPost = function(txtCoder,txtPost){
+      this.posts.push({id: this.postCount,
+                       txtCoder: txtCoder,
+                       textPost: txtPost
+                     });
+      this.postCount++;
+  }
+  this.printPost = function (parent) {
+    parent.innerHTML = "";
+    this.posts.forEach(function(post){
+      var firstPost = parent.firstElementChild;
+      //console.log(parent);
+      //console.log(this.createPost(post.txtCoder,post.textPost,post.id));
+      parent.insertBefore(this.createPost(post.txtCoder,post.textPost,post.id),firstPost);
+    },this);
+  }
+
+  this.createPost = function (textCoder, txtPost,id) {
+    var df = document.createDocumentFragment();
+    var post = document.createElement('div');
+    post.setAttribute("class","post");
+    var a = document.createElement('a');
+    var textoCoder = document.createTextNode(textCoder);
+    a.appendChild(textoCoder);
+    a.classList.add("textCoder");
+    var p = document.createElement('p');
+    var textoPost = document.createTextNode(txtPost);
+    p.appendChild(textoPost);
+    p.classList.add("textPost");
+
+    post.appendChild(a);
+    post.appendChild(p);
+
+    df.appendChild(post);
+    return df;
+    
   }
 
 }
+window.addEventListener('load', function () {
+    var postCoder = new PostCoder();
+    var btn_post = document.getElementById("btn_post");
 
-var btn_post = document.getElementById("btn_post");
-var posts = [];
+    btn_post.addEventListener("click", function(e){
+      e.preventDefault();
+      var txt_coderName = document.getElementById('txt_coderName');
+      var txt_post = document.getElementById('txt_post');
+      var resultado= document.getElementById('resultado');
 
-function Post(post){
-  this.post = post;
-}
-
-btn_post.addEventListener("click", function(e){
-  e.preventDefault();
-  var txt_post = document.getElementById('txt_post');
-  var dcf = document.createDocumentFragment();
-
-  var
+      postCoder.addPost(txt_coderName.value,txt_post.value);
+      postCoder.printPost(resultado);
+    });
 });
